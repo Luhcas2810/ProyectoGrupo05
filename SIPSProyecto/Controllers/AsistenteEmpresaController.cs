53f4fdc5941aeebb9e4c1f45d99847c41cfc1da0
scoti0309 <sebastian.valdivia@unmsm.edu.pe>
2024-07-18 01:50:36 -0500

    melva tmre

---
+++ SIPSProyecto/Controllers/AsistenteEmpresaController.cs
@@ -0,0 +1,91 @@
+﻿using SIPSProyecto.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SIPSProyecto.Controllers
+{
+    public class AsistenteEmpresaController : Controller
+    {
+        // GET: AsistenteEmpresa
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: AsistenteEmpresa/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: AsistenteEmpresa/Create
+        public ActionResult Create()
+        {
+
+            return View();
+        }
+
+        // POST: AsistenteEmpresa/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AsistenteEmpresa/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: AsistenteEmpresa/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AsistenteEmpresa/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: AsistenteEmpresa/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ SIPSProyecto/Controllers/TareaController.cs
@@ -0,0 +1,103 @@
+﻿using SIPSProyecto.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SIPSProyecto.Controllers
+{
+    public class TareaController : Controller
+    {
+        // GET: Tarea
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Tarea/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Tarea/Create
+        public ActionResult Create(Tarea actividad)
+        {
+            using (DBModels context = new DBModels())
+            {
+                if (actividad == null)
+                {
+                    return View();
+                }
+                var estudiantes = context.Estudiante.Select(e => new
+                {
+                    est_iCodigo = e.est_iCodigo,
+                    est_vcCodigo = e.est_vcCodigo
+                }).ToList();
+                ViewBag.Estudiantes = new SelectList(estudiantes, "est_iCodigo", "est_vcCodigo");
+                return View(actividad);
+            }
+        }
+
+        // POST: Tarea/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Tarea/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Tarea/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Tarea/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Tarea/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
